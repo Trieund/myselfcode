f75e9abef5832a976683ca68b7ac03c23ed29f09
go <go@192.168.1.26>
2017-11-28 15:48:53 +0900

    sua lan n

--- TestGit/src/test/git/HelloGit.java
+++ TestGit/src/test/git/HelloGit.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("Hello world");
 	System.out.println("hajimete to git");
 	System.out.println("i want to sua lan 2");
+	System.out.println("sua lan n");
 }
 }
