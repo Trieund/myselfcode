031eaabd530593121dcba8891b7bb5479cd4c180
go <go@192.168.1.26>
2017-11-29 22:46:37 +0900

    test2

---
+++ SuuChi/src/suuchi/atsukau/ZenMondaiApp.java
@@ -0,0 +1,33 @@
+package suuchi.atsukau;
+
+public class ZenMondaiApp {
+
+	public static void main(String[] args) {
+
+//		BigDecimalShori bds=new BigDecimalShori();
+//		System.out.println("Result of divice two number "+bds.wariBigDecimal());
+
+//		NamaeNuuRyoku nnr=new NamaeNuuRyoku();
+//		nnr.shoriNamae();
+//		nnr.nyuryokuHikisuu();
+//		nnr.test1();
+
+
+//		Kyukyu_NiJigenHairetsu kk=new Kyukyu_NiJigenHairetsu();
+//		kk.kyukyuNiJigenHairetsuNyuryoku();
+//		kk.kyukyuNiJigenhairetsuSutsuryoku();
+
+		SeisekiHyouka sh=new SeisekiHyouka();
+		sh.nyuryokuSeisekiHyouji();
+		sh.shutsuryokuSeisekiHyouji();
+		sh.nyuryokuHyoukaTen();
+		sh.shutsuryokuHyoukaTen();
+		sh.shuriSeisekiHyouka();
+		sh.shutsuryokuHyouka();
+
+//		sh.test2();
+//		Mondai6 md=new Mondai6();
+//		md.shoriKansuu();
+	}
+
+}
