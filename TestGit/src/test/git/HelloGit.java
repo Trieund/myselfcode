bcce53a4c5f2078e434710843f0ef284a471f51b
go <go@192.168.1.26>
2017-11-29 22:43:43 +0900

    京阪線

--- TestGit/src/test/git/HelloGit.java
+++ TestGit/src/test/git/HelloGit.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 	System.out.println("i want to sua lan 2");
 	System.out.println("da sua lan n");
 	System.out.println("京橋肥後橋naoru");
-	
+	System.out.println("肥後橋ダイビル");
+	System.out.println("京阪線");
 }
 }
